fff820f3c8589293ccef2133e331a8917fcb0c3d
Jose Luis Garrido Castro <joseluis.garrido@abamobile.com>
2026-01-20 19:41:02 +0100

    problema despliegue backend

--- Codigo/Backend/index.js
+++ Codigo/Backend/index.js
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:8100",
+    "http://localhost",
     "https://selugc4.github.io",
     "https://ritmoapp.onrender.com"
   ],
