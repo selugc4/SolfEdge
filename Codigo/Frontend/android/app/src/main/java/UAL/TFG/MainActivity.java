80376c572496e4ea774d02e3eb9cc2b97491a233
Jose Luis Garrido Castro <joseluis.garrido@abamobile.com>
2026-01-20 19:22:38 +0100

    correccion despliegue

---
+++ Codigo/Frontend/android/app/src/main/java/UAL/TFG/MainActivity.java
@@ -0,0 +1,5 @@
+package UAL.TFG;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ Codigo/Frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'UAL.TFG',
+  appName: 'SolfEdge',
+  webDir: 'www'
+};
+
+export default config;
