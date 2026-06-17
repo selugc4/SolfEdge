cfe09288fe7ca7122bae136b5356b9b57cac54ec
selugc4 <selugc4@users.noreply.github.com>
2026-06-17 20:48:00 +0000

    deploy: 0befdc52769b5ccff6575c6821b1a9cae30b7fa0

--- chunk-GUQ2J2YB.js
+++
@@ -1,1 +0,0 @@
-import{a as o,o as r}from"./chunk-XIH2NNB3.js";var n=(()=>{let t=class t{constructor(){this.tareaModifiedSource=new o,this.tareaModified$=this.tareaModifiedSource.asObservable()}touch(){this.tareaModifiedSource.next()}};t.\u0275fac=function(i){return new(i||t)},t.\u0275prov=r({token:t,factory:t.\u0275fac,providedIn:"root"});let e=t;return e})();export{n as a};
