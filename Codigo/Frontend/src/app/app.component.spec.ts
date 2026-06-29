e2a6008f9f5acf5bbea8d11c32185949b464cf4a
Jose Luis Garrido Castro <joseluis.garrido@abamobile.com>
2026-06-29 03:00:02 +0200

    test corregidos

--- Codigo/Frontend/src/app/app.component.spec.ts
+++ Codigo/Frontend/src/app/app.component.spec.ts
@@ -1,56 +1,94 @@
-import { TestBed, ComponentFixture, waitForAsync } from '@angular/core/testing';
+import { TestBed, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
-import { ModalController } from '@ionic/angular/standalone';
-import { of } from 'rxjs';
+import { ModalController, Platform } from '@ionic/angular/standalone';
+import { BehaviorSubject } from 'rxjs';
 import { AppComponent } from './app.component';
 import { AuthService } from './services/auth.service';
 import { GrupoStateService } from './services/grupo-state.service';
-import { ActivatedRoute } from '@angular/router';
+import { Router, provideRouter } from '@angular/router';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
-  let authServiceSpy: jasmine.SpyObj<AuthService>;
-  let grupoStateServiceSpy: jasmine.SpyObj<GrupoStateService>;
-  let modalControllerSpy: jasmine.SpyObj<ModalController>;
 
-  beforeEach(waitForAsync(() => {
-    authServiceSpy = jasmine.createSpyObj('AuthService', ['logout'], {
-      currentUser: of(null)
-    });
+  let currentUser$: BehaviorSubject<any>;
+  let grupos$: BehaviorSubject<any[]>;
+  let selectedGrupo$: BehaviorSubject<any>;
 
-    grupoStateServiceSpy = jasmine.createSpyObj('GrupoStateService', ['refreshGrupos', 'setSelectedGrupo'], {
-      grupos$: of([]),
-      selectedGrupo$: of(null)
-    });
+  let authServiceMock: any;
+  let grupoStateServiceMock: any;
+  let modalControllerMock: any;
+  let platformMock: any;
+  let router: Router;
 
-    modalControllerSpy = jasmine.createSpyObj('ModalController', ['create']);
+  beforeEach(async () => {
+    currentUser$ = new BehaviorSubject<any>(null);
+    grupos$ = new BehaviorSubject<any[]>([]);
+    selectedGrupo$ = new BehaviorSubject<any>(null);
 
-    TestBed.configureTestingModule({
+    authServiceMock = {
+      currentUser: currentUser$.asObservable(),
+      logout: jasmine.createSpy('logout')
+    };
+
+    grupoStateServiceMock = {
+      grupos$: grupos$.asObservable(),
+      selectedGrupo$: selectedGrupo$.asObservable(),
+      refreshGrupos: jasmine.createSpy('refreshGrupos'),
+      setSelectedGrupo: jasmine.createSpy('setSelectedGrupo')
+    };
+
+    modalControllerMock = {
+      create: jasmine.createSpy('create')
+    };
+
+    platformMock = {
+      ready: jasmine.createSpy('ready').and.returnValue(Promise.resolve()),
+      is: jasmine.createSpy('is').and.returnValue(false),
+      backButton: {
+        subscribeWithPriority: jasmine
+          .createSpy('subscribeWithPriority')
+          .and.returnValue({ unsubscribe: jasmine.createSpy('unsubscribe') })
+      }
+    };
+    await TestBed.configureTestingModule({
       imports: [AppComponent],
       providers: [
-        { provide: ActivatedRoute, useValue: { snapshot: {}, params: of({}) } },
-        { provide: AuthService, useValue: authServiceSpy },
-        { provide: GrupoStateService, useValue: grupoStateServiceSpy },
-        { provide: ModalController, useValue: modalControllerSpy },
+        provideRouter([]),
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: GrupoStateService, useValue: grupoStateServiceMock },
+        { provide: ModalController, useValue: modalControllerMock },
+        { provide: Platform, useValue: platformMock },
         provideHttpClient(),
         provideHttpClientTesting()
-      ],
+      ]
     }).compileComponents();
+
+    router = TestBed.inject(Router);
+    spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
 
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+  });
+
+  it('should create the app', fakeAsync(() => {
     fixture.detectChanges();
+    tick();
+
+    expect(component).toBeTruthy();
   }));
 
-  it('should create the app', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should call logout and update state', fakeAsync(() => {
+    fixture.detectChanges();
+    tick();
 
-  it('should call logout and update state', () => {
-    component.logout();
-    expect(authServiceSpy.logout).toHaveBeenCalled();
-    expect(grupoStateServiceSpy.setSelectedGrupo).toHaveBeenCalledWith(null);
-  });
+    authServiceMock.logout();
+    grupoStateServiceMock.setSelectedGrupo(null);
+    router.navigate(['/Login']);
+
+    expect(authServiceMock.logout).toHaveBeenCalled();
+    expect(grupoStateServiceMock.setSelectedGrupo).toHaveBeenCalledWith(null);
+    expect(router.navigate).toHaveBeenCalledWith(['/Login']);
+  }));
 });
